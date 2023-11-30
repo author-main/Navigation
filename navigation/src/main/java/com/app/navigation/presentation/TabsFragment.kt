c87309b9dddd070c29b7ce3dcc3de2bb318774c4
author-main <author-main@mail.ru>
2023-11-30 11:13:40 +0300

    Navigation 29-11-2023

--- navigation/src/main/java/com/app/navigation/presentation/TabsFragment.kt
+++ navigation/src/main/java/com/app/navigation/presentation/TabsFragment.kt
@@ -6,15 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.navigation.R
+import com.app.navigation.presentation.navigation.NavigationMode
+import com.app.navigation.presentation.navigation.NavigationModeHolder
+import javax.inject.Inject
 
 class TabsFragment : Fragment() {
 
-    override fun onCreateView(
+    @Inject
+    lateinit var navigationModeHolder: NavigationModeHolder
+
+   /* override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tabs, container, false)
-    }
+    }*/
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (navigationModeHolder.navigationMode is NavigationMode.Tabs) {
+
+        }
+    }
 }
---
+++ navigation/src/main/java/com/app/navigation/presentation/navigation/NavigationModeHolder.kt
@@ -0,0 +1,9 @@
+package com.app.navigation.presentation.navigation
+
+import javax.inject.Inject
+
+class NavigationModeHolder @Inject constructor() {
+
+    var navigationMode: NavigationMode = NavigationMode.Stack
+
+}
